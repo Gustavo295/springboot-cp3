66e3f5d6d4bb5edabb89295b6d243fc9c000e923
Gustavo Matias <gustavomteixeira.2006@gmail.com>
2025-02-21 12:04:39 -0300

    aula03

---
+++ src/main/java/br/com/fiap/api_rest/model/Categoria.java
@@ -0,0 +1,16 @@
+package br.com.fiap.api_rest.model;
+
+public enum Categoria {
+    BRONZE("Bronze"),
+    PRATA("Prata"),
+    OURO("Ouro");
+
+    private String descricao;
+
+    Categoria(String descricao) {
+        this.descricao = descricao;
+    }
+    public String getDescricao(){
+        return descricao;
+    }
+}
