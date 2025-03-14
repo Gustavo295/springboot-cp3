2309b03e8a120f5fb7e3f5e30d13d991590c921c
Gustavo295 <gustavomteixeira.2006@gmail.com>
2025-03-14 08:40:15 -0300

    aula05 import

--- src/main/java/br/com/fiap/api_rest/ApiRestApplication.java
+++ src/main/java/br/com/fiap/api_rest/ApiRestApplication.java
@@ -1,9 +1,11 @@
 package br.com.fiap.api_rest;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootNPSApplication
 @OpenAPIDefinition(info =
 @Info(title = "API de Clientes", description = "Exemplo de API RESTful com Swagger", version = "v1))
 public class ApiRestApplication {
